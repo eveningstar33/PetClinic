c0f0a089a33fac0870013b87fa0a771557326073
Dragos Sarpe <contact@usdgadget.com>
2018-08-24 10:30:23 +0300

    Initial import of Java project

---
+++ src/main/java/com/dgs/springbootpetclinic/SpringbootPetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.dgs.springbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetclinicApplication.class, args);
+    }
+}
