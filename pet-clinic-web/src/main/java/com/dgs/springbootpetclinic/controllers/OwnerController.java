ec477370d1ba70d912a8060093740dae73f86eb3
Dragos Sarpe <contact@usdgadget.com>
2018-08-25 17:04:13 +0300

    Added Owners list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/dgs/springbootpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/dgs/springbootpetclinic/controllers/OwnerController.java
@@ -1,13 +1,26 @@
 package com.dgs.springbootpetclinic.controllers;
 
+import com.dgs.springbootpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    // I'm declaring it final so that object can never be changed
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
