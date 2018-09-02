428135904f1b7dc0a119f623d387fcaf166037bd
Dragos Sarpe <contact@usdgadget.com>
2018-09-02 12:19:48 +0300

    Added specialities to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/dgs/springbootpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.dgs.springbootpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String descirption;
+
+    public String getDescirption() {
+        return descirption;
+    }
+
+    public void setDescirption(String descirption) {
+        this.descirption = descirption;
+    }
+}
