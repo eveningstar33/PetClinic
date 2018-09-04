bf50d8a8aca7e90f77c03e6d5ea91b3fa3171fe3
Dragos Sarpe <contact@usdgadget.com>
2018-09-04 22:33:58 +0300

    Added Spring Data JPA service for Speciality. Closes #44

---
+++ pet-clinic-data/src/main/java/com/dgs/springbootpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,44 @@
+package com.dgs.springbootpetclinic.services.springdatajpa;
+
+import com.dgs.springbootpetclinic.model.Speciality;
+import com.dgs.springbootpetclinic.repositories.SpecialtyRepository;
+import com.dgs.springbootpetclinic.services.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialitySDJpaService implements SpecialityService {
+
+    public SpecialitySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    private final SpecialtyRepository specialtyRepository;
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
